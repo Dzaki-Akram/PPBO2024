3dc7d65ad027ea5d82b3ea53223a1c1cd1026a05
Dzaki-Akram <h1101231056@student.untan.ac.id>
2024-09-02 18:56:00 +0700

    Praktikum 2B - Menggunakan Fungsi Bawaan

--- filepertama.php
+++ filepertama.php
@@ -1,5 +1,25 @@
 <?php
 
-echo "Hello World";
+date_default_timezone_set("Asia/Jakarta");
+
+$nama = readline('Masukkan nama Anda: ');
+$waktu = date('H');
+$sapaan = "";
+
+if ($waktu >= 5) {
+    if ($waktu < 11) {
+        $sapaan = "Selamat pagi";
+    } elseif ($waktu < 15) {
+        $sapaan = "Selamat siang";
+    } elseif ($waktu < 19) {
+        $sapaan = "Selamat sore";
+    } else {
+        $sapaan = "Selamat malam";
+    }
+} else {
+    $sapaan = "Selamat malam";
+}
+
+echo "{$sapaan}, {$nama}! Sekarang pukul " . date('H:i') . "\n";
 
 ?>
